e79f56fd42a244febec7e92ab06840efa98bfc65
root <sunrise1048@gmail.com>
2023-02-01 16:56:38 -0500

    sent_to_bulb

--- Kafka_python/sentiment_to_bulb.py
+++ Kafka_python/sentiment_to_bulb.py
@@ -1,18 +1,14 @@
 from kafka import KafkaConsumer
 
-EXTERNAL_IP = '35.236.200.251'
+EXTERNAL_IP = '34.132.166.200'
 PORT = '9092'
 
 consumer = KafkaConsumer(
     'sentiment_test',
-    bootstrap_servers = [EXTERNAL_IP+':'+PORT],
-    group_id = 'bulb-app'
+    bootstrap_servers = [EXTERNAL_IP+':'+PORT]
     )
 
 
 for message in consumer:
     message = message.value.decode('utf-8')
     print(message)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(bulbAction(message))
