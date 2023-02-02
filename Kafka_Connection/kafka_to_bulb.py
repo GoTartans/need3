b53fc70ca442e5e13e1ba15a7b19103b09de6939
root <sunrise1048@gmail.com>
2023-02-01 19:29:19 -0500

    updating

--- Kafka_Connection/kafka_to_bulb.py
+++ Kafka_Connection/kafka_to_bulb.py
@@ -1,13 +1,19 @@
 from kafka import KafkaConsumer
 
-EXTERNAL_IP = '34.132.166.200'
+# info of the instance where kafka cluster is located
+EXTERNAL_IPs = {
+    'team3-gpu':'35.184.175.239',
+    'instance-team3':'34.145.236.124',
+    'Chanwoo':'34.132.166.200',
+}
+EXTERNAL_IP = EXTERNAL_IPs['team3-gpu']
 PORT = '9092'
+TOPIC_NAME = 'sentiment_test'
 
 consumer = KafkaConsumer(
-    'sentiment_test',
+    TOPIC_NAME,
     bootstrap_servers = [EXTERNAL_IP+':'+PORT]
     )
-
 
 for message in consumer:
     message = message.value.decode('utf-8')
--- Kafka_Connection/mic_to_kafka.py
+++ Kafka_Connection/mic_to_kafka.py
@@ -1,26 +1,60 @@
 from kafka import KafkaProducer
 
-EXTERNAL_IP = '34.132.166.200'
+# info of the instance where kafka cluster is located
+EXTERNAL_IPs = {
+    'team3-gpu':'35.184.175.239',
+    'instance-team3':'34.145.236.124',
+    'Chanwoo':'34.132.166.200',
+}
+EXTERNAL_IP = EXTERNAL_IPs['team3-gpu']
 PORT = '9092'
 
 producer = KafkaProducer(
     bootstrap_servers=[EXTERNAL_IP+':'+PORT]
 )
 
+import speech_recognition as sr
+
+r = sr.Recognizer()
+#sr.Microphone.list_microphone_names()
+mic = sr.Microphone()
+
+# Derived from https://realpython.com/python-speech-recognition/
+def recognize_speech_from_mic(recognizer, microphone):
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+    
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        audio = recognizer.listen(source,phrase_time_limit=3)
+        
+        try:
+            audio_wav = audio.get_wav_data(convert_rate=microphone.SAMPLE_RATE)
+        except sr.UnknownValueError:
+            audio_wav = None
+
+    return(audio_wav)
+
+
 run_app = True
 i = 1
 
 TOPIC_NAME = 'wav_test'
 while run_app:
-    print('Line - ' + str(i))
-    output = input('wav example:')
-    i = i + 1
+    output = recognize_speech_from_mic(r,mic)
 
-    producer.send(TOPIC_NAME,
-        key=b'voice', 
-        value=bytes(output, 'utf-8')
-    )
+    if output is not None:
+        print('Line - ' + str(i))
+        producer.send(TOPIC_NAME,
+            key=b'voice', 
+            value=bytes(output)#, 'utf-8')
+        )
 
     if output == 'stop Voice': 
         run_app = False
     
+    i = i + 1
